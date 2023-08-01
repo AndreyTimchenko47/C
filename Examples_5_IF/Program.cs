00837617fdc78ad7bf80bbae2335aa1ad429ac3f
Andrey <Andrey@mail.ru>
2023-08-01 21:57:31 +0300

    Сделали программу для выявления определённого но с русским не работает

---
+++ Examples_5_IF/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower () == "masha")
+{
+    Console.WriteLine("Это Она");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
